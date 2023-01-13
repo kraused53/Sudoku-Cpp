81fd6115cb9b458ca4ada76997e6e190c7831e63
kraused53 <kraused53@gmail.com>
2023-01-13 17:12:46 -0500

    reset commit cache

--- bitmasks.h
+++
@@ -1,8 +0,0 @@
-#ifndef BITMASK_H
-    #define BITMASK_H
-
-    #define ORIGINAL_BITMASK    0x80 // Use to mark an original number
-    #define ROW_SELECT          0x40 // Use to mark selected row
-    #define COL_SELECT          0x20 // Use to mark selected col
-    #define NUMBER_MASK         0x0F // Use to isolate a cell's number
-#endif
